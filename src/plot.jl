8fc23470f976b8f166d65756702d97f3e0cecec9
holowaczi <igor.holowacz@gmail.com>
2023-05-29 09:57:21 +0200

    generated trajectories for T=2.26

--- src/plot.jl
+++ src/plot.jl
@@ -13,7 +13,7 @@ heatmap(state80_T2_26, title ="L=80, T=2.26", legend = false)
 heatmap(state80_T4_0, title ="L=80, T=4", legend = false)
 ####
 #Ploting trajectories
-@save "data/trajectories_T1_0.jld2" trajectories_L10_T1_0 trajectories_L20_T1_0 trajectories_L40_T1_0 trajectories_L80_T1_0
+@load "data/trajectories_T1_0.jld2" trajectories_L10_T1_0 trajectories_L20_T1_0 trajectories_L40_T1_0 trajectories_L80_T1_0
 
 plot(trajectories_L10_T1_0, legend=false, xlabel = "MCS", ylabel="m", title="L=10, T=1")
 plot(trajectories_L20_T1_0, legend=false, xlabel = "MCS", ylabel="m", title="L=20, T=1")
