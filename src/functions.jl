adfe1fa88932543492661a8e1adf535e2486b1c7
holowaczi <igor.holowacz@gmail.com>
2023-05-29 09:23:05 +0200

    trajectory random and order

--- src/functions.jl
+++ src/functions.jl
@@ -26,9 +26,19 @@ function metropolis_with_field!(S, T, h, L)
     end
 end
 
-function trajectory(L,T,MSC)
+function trajectory_random(L,T,MSC)
     m = Vector{Float64}(undef,MSC)
     S = rand((-1,1),L,L)
+    for step in 1:MSC
+        metropolis!(S,T,L)
+        m[step] = mean(S)
+    end
+    m
+end
+
+function trajectory_order(L,T,MSC)
+    m = Vector{Float64}(undef,MSC)
+    S = ones(L,L)
     for step in 1:MSC
         metropolis!(S,T,L)
         m[step] = mean(S)
@@ -45,7 +55,7 @@ function state(L,T,MSC)
     S
 end
 
-function animate_ising(L,T,MSC)
+function animate_ising(L,T,MSC, path::String, FPS::Int64)
     m = Vector{Float64}(undef,MSC)
     S = rand((-1,1),L,L)
     an = @animate for step in ProgressBar(1:MSC)
@@ -56,5 +66,5 @@ function animate_ising(L,T,MSC)
         plot(p1,p2, layout= grid(2, 1, heights=[0.8 ,0.2]))
     end
     plot!(size=(700,1000))
-    gif(an, "Ising.gif", fps = 25)
+    gif(an, path, fps = FPS)
 end
---
+++ src/results.jl
@@ -0,0 +1,5 @@
+using Random, Plots, ProgressBars, Statistics, StatsBase
+
+include("functions.jl")
+
+m10 = trajectory_random(10,1,10^4)
