bf74209629ab505fb8e28b22bc98622b8aec4bc6
holowaczi <igor.holowacz@gmail.com>
2023-05-29 09:31:52 +0200

    generated trajectories for T=1

--- src/results.jl
+++ src/results.jl
@@ -1,5 +1,17 @@
-using Random, Plots, ProgressBars, Statistics, StatsBase
+using Random, Plots, ProgressBars, Statistics, StatsBase, JLD2, FileIO
 
 include("functions.jl")
 
-m10 = trajectory_random(10,1,10^4)
+trajectories_L10_T1_0 = Vector(undef,10)
+trajectories_L20_T1_0 = Vector(undef,10)
+trajectories_L40_T1_0 = Vector(undef,10)
+trajectories_L80_T1_0 = Vector(undef,10)
+
+for i in 1:10
+    trajectories_L10_T1_0[i] = trajectory_random(10,1,100)
+    trajectories_L20_T1_0[i] = trajectory_random(20,1,100)
+    trajectories_L40_T1_0[i] = trajectory_random(40,1,100)
+    trajectories_L80_T1_0[i] = trajectory_random(80,1,100)
+end
+
+@save "data/trajectories_T1_0.jld2" trajectories_L10_T1_0 trajectories_L20_T1_0 trajectories_L40_T1_0 trajectories_L80_T1_0
